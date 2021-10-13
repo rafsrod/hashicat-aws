c17e72a162f2dcd898130f86594116e64e1b6f85
Rafael Sabaudo <rafael.sabaudo@novanext.it>
2021-10-13 11:02:06 +0000

    added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "2.2.0"
-  prefix = rafaelsabaudo
+  bucket_prefix = rafaelsabaudo
   # insert the 5 required variables here
 }
